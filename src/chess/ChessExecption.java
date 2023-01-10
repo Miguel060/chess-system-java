bd0cbadf4b5e82aafb9c4a04c687b7ef15a0877f
Miguel Doiche <migueldoiche2@gmail.com>
2023-01-10 19:36:34 -0300

    Moving pieces

--- src/chess/ChessExecption.java
+++
@@ -1,9 +0,0 @@
-package chess;
-
-public class ChessExecption extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	public ChessExecption(String msg) {
-		super(msg);
-	}
-
-}
